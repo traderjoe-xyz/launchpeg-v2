5a5bed03141148991993fe0e9c32ddcf441a4b3d
SK Sim <simsk87@gmail.com>
2022-11-08 00:46:28 +0800

    update deployment scripts

--- deploy/LaunchpegLens.ts
+++ deploy/LaunchpegLens.ts
@@ -1,21 +1,34 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
+import { getLaunchpegFactoryV1 } from './utils'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getChainId, getNamedAccounts, run } = hre
   const { deploy } = deployments
-
   const { deployer } = await getNamedAccounts()
+  const chainId = await getChainId()
 
-  const launchpegFactoryAddress = (await deployments.get('LaunchpegFactory')).address
+  const factoryV1Address = getLaunchpegFactoryV1(chainId)
+  const factoryV2Address = (await deployments.get('LaunchpegFactory')).address
+  const batchRevealAddress = (await deployments.get('BatchReveal')).address
 
-  await deploy('LaunchpegLens', {
+  const constructorArgs = [factoryV1Address, factoryV2Address, batchRevealAddress]
+  const deployResult = await deploy('LaunchpegLens', {
     from: deployer,
-    args: [launchpegFactoryAddress],
+    args: constructorArgs,
     log: true,
     autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
   })
+
+  try {
+    await run('verify:verify', {
+      address: deployResult.address,
+      constructorArguments: constructorArgs,
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
 export default func
 func.tags = ['LaunchpegLens']
-func.dependencies = ['LaunchpegFactory']
+func.dependencies = ['LaunchpegFactory', 'BatchReveal']
