8756af31ad34497bdba3bcfb9e2de69ba43bbb8a
Patate <patate@joepegs.com>
2023-04-06 20:30:28 +0200

    fix deploy scripts

--- tasks/utils.ts
+++ tasks/utils.ts
@@ -45,7 +45,7 @@ export const getProxyOwner = (chainId: string): string => {
   } else if (chainId === '43114' || chainId === '31337') {
     return '0x64c4607AD853999EE5042Ba8377BfC4099C273DE'
   } else {
-    throw `Unknown chain ID ${chainId}`
+    return '0x0000000000000000000000000000000000000000'
   }
 }
 
@@ -55,6 +55,6 @@ export const getLaunchpegFactoryV1 = (chainId: string): string => {
   } else if (chainId === '43114') {
     return '0x7BFd7192E76D950832c77BB412aaE841049D8D9B'
   } else {
-    throw `Unknown chain ID ${chainId}`
+    return '0x0000000000000000000000000000000000000000'
   }
 }
